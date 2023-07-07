35c1ce700751172140705b4b6d99e33075b6afff
Monica <maaylahuerta@gmail.com>
2023-07-07 14:12:55 -0400

    Inicio desafío condiciones

---
+++ assets/js/script.js
@@ -0,0 +1,52 @@
+function cambioColorBorde() {
+    let imagen = document.querySelector('#imagen_conejo')
+    // imagen.style.border = '10px solid red'
+    if(imagen.style.borderColor !== 'red'){
+        imagen.style.border = '2px solid red'
+    }
+    else{
+        imagen.style.border = '0px solid black'
+    }
+    
+}
+
+function contrasenya() {
+    let elemento1 = document.querySelector('.selector_uno').value
+    let elemento2 = document.querySelector('.selector_dos').value
+    let elemento3 = document.querySelector('.selector_tres').value
+   
+    let contrasenya = elemento1 + elemento2 + elemento3
+    if(elemento1 !== 'Selecciona un número' && elemento2 !== 'Selecciona un número' && elemento3 !== 'Selecciona un número'){
+        if (contrasenya == 911){
+            alert('Password 1 correcto')
+        }
+        else if ( contrasenya == 714){
+            alert('Password 2 correcto')
+        }
+        else{
+            alert('Password incorrecto')
+        }
+    } 
+    else{
+        alert('Ingresa la contraseña de 3 dígitos')
+    }   
+}
+
+function verificar() {
+    let elemento1 = document.querySelector('.input__uno').value
+    let elemento2 = document.querySelector('.input__dos').value
+    let elemento3 = document.querySelector('.input__tres').value
+    let suma = Number(elemento1) + Number(elemento2) + Number(elemento3)
+    let parrafo = document.querySelector('p')
+    if(suma >= 10) {
+     parrafo.innerHTML = 'Llevas demasiados stickers'
+ 
+    }
+    else if(suma == 1) {
+     parrafo.innerHTML = 'LLevas ' + suma + ' sticker'
+    }
+    else{
+        parrafo.innerHTML = 'LLevas ' + suma + ' stickers'
+    }
+     
+ }
