e2b61f66f0b96165b9958e01a22df3c1957ff534
Monica <maaylahuerta@gmail.com>
2023-07-07 14:17:49 -0400

    Se agregan validaciones a método verificarStickers

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,17 +14,18 @@ function contrasenya() {
     let elemento1 = document.querySelector('.selector_uno').value
     let elemento2 = document.querySelector('.selector_dos').value
     let elemento3 = document.querySelector('.selector_tres').value
+    const texto = document.querySelector('p')
    
     let contrasenya = elemento1 + elemento2 + elemento3
     if(elemento1 !== 'Selecciona un número' && elemento2 !== 'Selecciona un número' && elemento3 !== 'Selecciona un número'){
         if (contrasenya == 911){
-            alert('Password 1 correcto')
+            texto.innerHTML = 'Password 1 correcto'
         }
         else if ( contrasenya == 714){
-            alert('Password 2 correcto')
+            texto.innerHTML = 'Password 2 correcto'
         }
         else{
-            alert('Password incorrecto')
+            texto.innerHTML = 'Password incorrecto'
         }
     } 
     else{
@@ -32,20 +33,24 @@ function contrasenya() {
     }   
 }
 
-function verificar() {
+function verificarStickers() {
     let elemento1 = document.querySelector('.input__uno').value
     let elemento2 = document.querySelector('.input__dos').value
     let elemento3 = document.querySelector('.input__tres').value
     let suma = Number(elemento1) + Number(elemento2) + Number(elemento3)
     let parrafo = document.querySelector('p')
-    if(suma >= 10) {
+    if(suma > 10) {
      parrafo.innerHTML = 'Llevas demasiados stickers'
  
     }
     else if(suma == 1) {
      parrafo.innerHTML = 'LLevas ' + suma + ' sticker'
     }
-    else{
+    else if(suma <= 0){
+        alert('Ingresa tus stickers')
+    }
+    else
+    {
         parrafo.innerHTML = 'LLevas ' + suma + ' stickers'
     }
      
